2adc888c87b460c03ac60b20929cfb14a72edcca
Ilman1512 <godkaris35@gmail.com>
2026-06-15 13:52:45 +0700

    Update buku_majalah.py

--- buku_majalah.py
+++ buku_majalah.py
@@ -4,14 +4,31 @@ class buku(perpustakaan):
     def __init__(self, kode, judul, tahun, pengarang, penerbit):
         super().__init__(kode, tahun, judul, penerbit)
         self.pengarang = pengarang
-    
+
     def tampilkan(self, no):
-        print(f"Koleksi nomor{no}: \n Jenis Koleksi: Buku \n Kode Koleksi: {self.kode} \n Judul Buku: {self.judul} \n Tahun terbit: {self.tahun} \n Pengarang: {self.pengarang} \n Penerbit: {self.penerbit} ")
+        print(f"""
+Koleksi {no}:
+Jenis        : Buku
+Kode Koleksi : {self.kode}
+Judul        : {self.judul}
+Thn Terbit   : {self.tahun}
+Pengarang    : {self.pengarang}
+Penerbit     : {self.penerbit}
+""")
+
 
 class majalah(perpustakaan):
     def __init__(self, kode, judul, tahun, edisi, penerbit):
         super().__init__(kode, tahun, judul, penerbit)
         self.edisi = edisi
-    
+
     def tampilkan(self, no):
-        print(f"Koleksi nomor{no}: \n Jenis Koleksi: Majalah \n Kode Koleksi: {self.kode} \n Judul Majalah: {self.judul} \n Tahun terbit: {self.tahun} \n Edisi: {self.edisi} ")
+        print(f"""
+Koleksi {no}:
+Jenis        : Majalah
+Kode Koleksi : {self.kode}
+Judul        : {self.judul}
+Tahun Terbit : {self.tahun}
+Penerbit     : {self.penerbit}
+Edisi        : {self.edisi}
+""")
