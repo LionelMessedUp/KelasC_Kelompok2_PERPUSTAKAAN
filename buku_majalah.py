c5529033729bad413b2ec8487a0d7beb9939d47b
Ilman1512 <godkaris35@gmail.com>
2026-06-15 15:04:33 +0700

    Update buku_majalah.py

--- buku_majalah.py
+++ buku_majalah.py
@@ -1,30 +1,36 @@
+from koleksi import Koleksi
+
 class Buku(Koleksi):
-    def __init__(self, kode, judul, tahun,
-                 pengarang, penerbit):
+    def __init__(self, kode, judul, tahun, pengarang, penerbit):
         super().__init__(kode, judul, tahun)
         self.pengarang = pengarang
         self.penerbit = penerbit
 
     def tampilkan(self):
-        print("\n=== DATA BUKU ===")
-        print("Kode       :", self.kode)
-        print("Judul      :", self.judul)
-        print("Tahun      :", self.tahun)
-        print("Pengarang  :", self.pengarang)
-        print("Penerbit   :", self.penerbit)
+        return (
+            f"Jenis : Buku\n"
+            f"Kode : {self.kode}\n"
+            f"Judul : {self.judul}\n"
+            f"Tahun : {self.tahun}\n"
+            f"Pengarang : {self.pengarang}\n"
+            f"Penerbit : {self.penerbit}"
+        )
 
+
+from koleksi import Koleksi
 
 class Majalah(Koleksi):
-    def __init__(self, kode, judul, tahun,
-                 penerbit, edisi):
+    def __init__(self, kode, judul, tahun, penerbit, edisi):
         super().__init__(kode, judul, tahun)
         self.penerbit = penerbit
         self.edisi = edisi
 
     def tampilkan(self):
-        print("\n=== DATA MAJALAH ===")
-        print("Kode       :", self.kode)
-        print("Judul      :", self.judul)
-        print("Tahun      :", self.tahun)
-        print("Penerbit   :", self.penerbit)
-        print("Edisi      :", self.edisi)
+        return (
+            f"Jenis : Majalah\n"
+            f"Kode : {self.kode}\n"
+            f"Judul : {self.judul}\n"
+            f"Tahun : {self.tahun}\n"
+            f"Penerbit : {self.penerbit}\n"
+            f"Edisi : {self.edisi}"
+        )
